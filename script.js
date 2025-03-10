9d7063762d1cabf5ead504c011237057507992d7
Ayush Gangwar <122666769+AyushGangwar019@users.noreply.github.com>
2025-03-10 17:05:59 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ const lenis = new Lenis({
 });
 
 // Listen for the scroll event and log the event data
-lenis.on('scroll', (e) => {
-  console.log(e);
-});
+// lenis.on('scroll', (e) => {
+//   console.log(e);
+// });
 
 const navbarLI = document.querySelectorAll(".navbar-li");
 
@@ -16,3 +16,95 @@ navbarLI.forEach(item => {
       item.classList.add("selected")
     })
   });
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+// Reusable scrollTrigger settings for odd elements
+const oddScrollTrigger = {
+  start: "top 100%",
+  end: "top 90%",
+  scrub: 6,
+  // markers: true
+};
+
+// Reusable scrollTrigger settings for even elements
+const evenScrollTrigger = {
+  start: "top 90%",
+  end: "top 80%",
+  scrub: 6,
+  // markers: true
+};
+
+// Reusable scrollTrigger settings for odd2 elements
+const odd2ScrollTrigger = {
+  start: "top 80%",
+  end: "top 70%",
+  scrub: 6,
+  // markers: true
+};
+
+// Timeline for animations
+const tl = gsap.timeline();
+
+// Odd element animations
+tl.from(".section2-image.odd", {
+  x: "20vw",
+  opacity: 0,
+  ease: "power2.inOut",
+  scrollTrigger: {
+    trigger: ".section2-img-description-container.container1",
+    ...oddScrollTrigger
+  }
+})
+.from(".section2-description.odd", {
+  x: "-20vw",
+  opacity: 0,
+  ease: "power2.inOut",
+  scrollTrigger: {
+    trigger: ".section2-img-description-container.container1",
+    ...oddScrollTrigger
+  }
+}, "<");
+
+// Even element animations
+tl.from(".section2-image.even", {
+  x: "-20vw",
+  opacity: 0,
+  ease: "power2.inOut",
+  scrollTrigger: {
+    trigger: ".section2-img-description-container.container2",
+    ...evenScrollTrigger
+  }
+})
+.from(".section2-description.even", {
+  x: "20vw",
+  opacity: 0,
+  ease: "power2.inOut",
+  scrollTrigger: {
+    trigger: ".section2-img-description-container.container2",
+    ...evenScrollTrigger
+  }
+}, "<");
+
+// Odd2 element animations
+tl.from(".section2-image.odd2", {
+  x: "20vw",
+  opacity: 0,
+  ease: "power2.inOut",
+  scrollTrigger: {
+    trigger: ".section2-img-description-container.container3",
+    ...odd2ScrollTrigger
+  }
+})
+.from(".section2-description.odd2", {
+  x: "-20vw",
+  opacity: 0,
+  ease: "power2.inOut",
+  scrollTrigger: {
+    trigger: ".section2-img-description-container.container3",
+    ...odd2ScrollTrigger
+  }
+}, "<");
+
+  
